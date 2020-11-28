deb7f212336e69fa8feb837e272666af43e7850a
Anya Lozovaya <anyalozovaya2014@gmail.com>
2020-11-29 01:47:07 +0400

    fix: fix all bags with links

--- script.js
+++ script.js
@@ -16,293 +16,95 @@ const linkEmotions = document.querySelector('.emotions')
 const linkBodyParts = document.querySelector('.body_parts')
 const linkMain = document.querySelector('.main_page')
 const linkColors = document.querySelector('.colors')
-console.log(linkColors)
 const linkFamily = document.querySelector('.family')
 
 fetch('/categories.json').then(res => res.json()).then(list => {
     categories = list
-    console.log(cards)
+
     createCategoriesOrWords(categories, nameOfcategorie)
+
     linkAnimal1.addEventListener('click', () => {
         nameOfcategorie = "animal 1"
         click(nameOfcategorie)
 
-        for (let i = 0; i < cards.length; i++) {
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
-
+        functionsOnCards()
     })
     linkAnimal2.addEventListener('click', () => {
         nameOfcategorie = "animal 2"
         click(nameOfcategorie)
-        for (let i = 0; i < cards.length; i++) {
-
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
     linkProfessions.addEventListener('click', () => {
         nameOfcategorie = "professions"
         click(nameOfcategorie)
-        for (let i = 0; i < cards.length; i++) {
-
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
 
     linkClothes.addEventListener('click', () => {
 
         nameOfcategorie = "clothes"
         click(nameOfcategorie)
-        for (let i = 0; i < cards.length; i++) {
-
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
 
     linkEmotions.addEventListener('click', () => {
         nameOfcategorie = "emotions"
         click(nameOfcategorie)
-        for (let i = 0; i < cards.length; i++) {
-
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
 
     linkBodyParts.addEventListener('click', () => {
         nameOfcategorie = "body parts"
         click(nameOfcategorie)
-        for (let i = 0; i < cards.length; i++) {
-
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
 
     linkColors.addEventListener('click', () => {
         nameOfcategorie = "colors"
         click(nameOfcategorie)
-        console.log('colors')
-        for (let i = 0; i < cards.length; i++) {
-
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
     linkFamily.addEventListener('click', () => {
         nameOfcategorie = "family"
         click(nameOfcategorie)
-        for (let i = 0; i < cards.length; i++) {
-
-
-            let tap = cards[i].children[0].children[0]
-            let sound = cards[i].children[0].children[0].children[1].innerHTML
-            tap.children[0].addEventListener('click', () => soundTurnOn(sound))
-            tap.children[1].addEventListener('click', () => soundTurnOn(sound))
-
-
-        }
-        const card = document.querySelectorAll('.translate')
-        console.log(card);
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', function() {
-
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-
-            });
-        }
-
-        const car = document.querySelectorAll('.box_of_cards__card--back');
-        console.log(car)
-        for (let i = 0; i < car.length; i++) {
-            car[i].addEventListener('mouseleave', () => {
-                let c = document.querySelectorAll('.card')
-                c[i].classList.toggle('is-flipped');
-            })
-        }
+        functionsOnCards()
     })
 
     linkMain.addEventListener('click', () => {
         nameOfcategorie = "main"
         click(nameOfcategorie)
-    })
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].addEventListener('click', function() {
 
+                let linkToCateg = cards[i].children[0].children[0].children[1].innerHTML.toLowerCase()
+
+                if (linkToCateg === 'animal i') {
+                    linkToCateg = 'animal 1'
+                } else if (linkToCateg === 'animal ii') {
+                    linkToCateg = 'animal 2'
+                }
+                console.log(linkToCateg)
+
+                click(linkToCateg)
+                functionsOnCards()
+            });
+        }
+    })
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', function() {
+            let linkToCateg = cards[i].children[0].children[0].children[1].innerHTML.toLowerCase()
+
+            if (linkToCateg === 'animal i') {
+                linkToCateg = 'animal 1'
+            } else if (linkToCateg === 'animal ii') {
+                linkToCateg = 'animal 2'
+            }
+            console.log(linkToCateg)
+
+            click(linkToCateg)
+            functionsOnCards()
+        });
+    }
 
 })
 
@@ -334,8 +136,8 @@ function createCategorieOrWord(array, name) {
                 <div class="card">
                     <div class="box_of_cards__card box_of_cards__card--front">
                         <img class="box_of_cards__card__image" alt="" src="${array[i].img}">
-                        <h2>${array[i].name}</h2>
-                        <audio data-key="sound_${array[i].name}" src="${array[i].sound}"></audio><button class="translate"></button>
+                        <h2 class="animal_1">${array[i].name}</h2>
+                        <audio data-key="sound_${array[i].name}" src="${array[i].sound}"></audio>
                     </div>
                 </div>
             </div>`
@@ -351,7 +153,7 @@ function createCategorieOrWord(array, name) {
                     </div>
                     <div class="box_of_cards__card box_of_cards__card--back">
                         <img class="box_of_cards__card__image " alt="" src="${array[i].img}">
-                        <h2>${array[i].name}</h2>
+                        <h2>${array[i].translate}</h2>
                         <audio data-key="sound_${array[i].name}" src="${array[i].sound}"></audio>
                     </div>
                 </div>
@@ -365,5 +167,37 @@ function soundTurnOn(child) {
     let audio = document.querySelector(`audio[data-key="sound_${child}"]`);
     audio.currentTime = 0;
     audio.play();
+}
+
+function functionsOnCards() {
+    for (let i = 0; i < cards.length; i++) {
+
+
+        let tap = cards[i].children[0].children[0]
+        let sound = cards[i].children[0].children[0].children[1].innerHTML
+        tap.children[0].addEventListener('click', () => soundTurnOn(sound))
+        tap.children[1].addEventListener('click', () => soundTurnOn(sound))
+
+
+    }
+    const card = document.querySelectorAll('.translate')
+    console.log(card);
+    for (let i = 0; i < card.length; i++) {
+        card[i].addEventListener('click', function() {
+
+            let c = document.querySelectorAll('.card')
+            c[i].classList.toggle('is-flipped');
+
+        });
+    }
+
+    const car = document.querySelectorAll('.box_of_cards__card--back');
+    console.log(car)
+    for (let i = 0; i < car.length; i++) {
+        car[i].addEventListener('mouseleave', () => {
+            let c = document.querySelectorAll('.card')
+            c[i].classList.toggle('is-flipped');
+        })
+    }
 }
 request.send()
